a2db0c8f202b311a35f007b882283a64965799e4
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-06 17:25:56 +0700

    Create the project

---
+++ AppleSignInFirebase/AppleSignInFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignInFirebaseApp.swift
+//  AppleSignInFirebase
+//
+//  Created by Danh Tu on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignInFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
