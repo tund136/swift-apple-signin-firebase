513e668d8522ecb4895389cc1b53cdf9c766837a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-06 17:35:58 +0700

    Add Home and Login View

---
+++ AppleSignInFirebase/View/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  AppleSignInFirebase
+//
+//  Created by Danh Tu on 06/10/2021.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
